14c2de6a4429af49088ee12407e0af2290ce39e6
Joe Kroesche <joe@kroesche.org>
2024-02-29 22:12:56 -0600

    Add range command for manually setting LEDs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cmdif
 import ws2812_pio as wspio
 
 ws = wspio.WS2812(0, 16)
-pixels = array.array("I", [0 for _ in range(144)])
+pixels = array.array("I", [0 for _ in range(144*3)])
 
 def meter(param_list, config_list):
     global pixels
@@ -20,9 +20,23 @@ def meter(param_list, config_list):
         pixels[dot0 + idx] = 0
     ws.show(pixels)
 
+def range(param_list, config_list):
+    global pixels
+    dot0 = int(param_list[1])
+    numdots = int(param_list[2])
+    red = int(param_list[3])
+    green = int(param_list[4])
+    blue = int(param_list[5])
+    color = green << 16
+    color += red << 8
+    color += blue
+    for idx in range(numdots):
+        pixels[dot0+idx] = color
+    ws.show(pixels)
+
 meter_config = [0, 40]
 
 ci = cmdif.CmdInterface()
 ci.add_cmd("meter", meter, "bar meter red to green (0-100)", meter_config)
-
+ci.add_cmd("range", range, "manual set range of LEDs (start,stop,r,g,b)", [])
 ci.run()
